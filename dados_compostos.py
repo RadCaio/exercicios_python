bf103bce31074380cd2f051fc4b38e798bfb92d4
RadCaio <caio.rochas@hotmail.com>
2023-05-11 17:21:28 -0300

    Create dados_compostos.py

---
+++ dados_compostos.py
@@ -0,0 +1,11 @@
+lista = [1,2,3,4,5]     # Lista (list) 
+print(lista)
+
+tupla = (1,2,3,4,5)     # Tupla (tuple) os valores na tupla não podem ser alterados 
+print(tupla) 
+
+conjunto = {1,2,3,2, 'repetindo', 'eu', 'repetindo', 'hello'}
+print(conjunto)         # Conjunto (set) os valores não podem se repetir e a ordem é aleatoria
+
+dicionario = {'RadCaio': 'O cara mais bonito da terra'} 
+print(dicionario) # Dicionário (dictionary) a nossa chave seria 'RadCaio' e o valor seria a descrição da nossa chave
